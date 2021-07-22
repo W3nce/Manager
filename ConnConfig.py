312a370eca51bbb330084713436b1d2de4f69037
Wence <86223302+W3nce@users.noreply.github.com>
2021-07-22 09:19:00 +0800

    Add files via upload

--- ConnConfig.py
+++ ConnConfig.py
@@ -2,16 +2,16 @@
 # username = 'sqladm' #root
 # password = 'sqlPW123' #pw
 
-# host = "localhost"
-# username = "root"
-# password = "password"
+#host = "localhost"
+#username = "root"
+#password = "password"
 
-host = "localhost"
-username = "root"
-password = "MWA1024"
+#host = "localhost"
+#username = "root"
+#password = "MWA1024"
 
-# host = "192.168.1.170"
-# username = "sqladm"
-# password = "MWApassword123"
+host = "192.168.1.170"
+username = "sqladm"
+password = "MWApassword123"
 
 database= ("INDEX_PRO_MASTER", "PUR_ORDER_MASTER", "INDEX_VEND_MASTER", "INDEX_EMP_MASTER", "COMPANY_INFO")
