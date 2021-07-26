d14287e688cfaed6e442eee96f48bc10de672e92
Wence <86223302+W3nce@users.noreply.github.com>
2021-07-26 12:04:40 +0800

    Update ConnConfig.py

--- ConnConfig.py
+++ ConnConfig.py
@@ -6,12 +6,12 @@
 #username = "root"
 #password = "password"
 
-host = "localhost"
-username = "root"
-password = "MWA1024"
+#host = "localhost"
+#username = "root"
+#password = "MWA1024"
 
-# host = "192.168.1.170"
-# username = "sqladm"
-# password = "MWApassword123"
+host = "192.168.1.170"
+username = "sqladm"
+password = "MWApassword123"
 
 database= ("INDEX_PRO_MASTER", "PUR_ORDER_MASTER", "INDEX_VEND_MASTER", "INDEX_EMP_MASTER", "COMPANY_INFO")
